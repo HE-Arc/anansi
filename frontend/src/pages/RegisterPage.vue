c8ec76d3021e76835f117a7e5902f785d11d8edd
Jeanne Michel <jeanne.michel@he-arc.ch>
2023-02-22 00:02:05 +0100

    Finished issue #3 : UI for login/register

--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -1,17 +1,74 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <register-form></register-form>
+  <q-page class="row q-mx-xl">
+    <div class="col-12 col-md-6 col-lg-4">
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+    <q-input
+      v-model="username"
+      label="Username"
+      type="text"
+      :rules="[val => !!val || 'Username is required']"
+    />
+    <q-input
+      v-model="email"
+      label="Email"
+      type="email"
+      :rules="[val => !!val || 'Email is required']"
+    />
+    <q-input
+      v-model="password"
+      label="Password"
+      type="password"
+      :rules="[val => !!val || 'Password is required']"
+    />
+    <q-input
+      v-model="password2"
+      label="Confirm Password"
+      type="password"
+      :rules="[val => !!val || 'Password is required']"
+    />
+    <q-btn type="submit" color="primary" label="Register" no-caps class="full-width q-my-md" />
+  </form>
+  <p class="text-blue-grey-4 q-pa-md">Have an account ? <q-btn class="q-ma-xs q-pa-xs" flat color="primary" :to="{name:'login'}" no-caps>Log in</q-btn></p>
+  </div>
   </q-page>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
-  import RegisterForm from 'components/RegisterForm.vue';
+import { defineComponent } from 'vue';
+import { useAuthStore } from 'src/stores/auth';
 
-  export default defineComponent({
-    name: 'RegisterPage',
-    components: {
-      RegisterForm
+export default defineComponent({
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+  },
+  setup() {
+    const authStore = useAuthStore();
+    return {
+      authStore
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        const response = await this.$axios.post('http://127.0.0.1:8000/api/register/', {
+          username: this.username,
+          email: this.email,
+          password: this.password,
+          password2: this.password2
+        });
+        console.log(response.data);
+        this.authStore.login();
+      } catch (error) {
+        console.log(error);
+      }
     }
-  });
+  }
+});
 </script>
