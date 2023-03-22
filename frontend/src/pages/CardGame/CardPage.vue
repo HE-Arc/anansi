f8ee71e3b760b5d7536805d8b03f18523f7e2174
Tan Dorian <dorian.tan@he-arc.ch>
2023-03-22 14:56:16 +0100

    Create CardPage.vue

---
+++ frontend/src/pages/CardGame/CardPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row justify-evenly content-start">
+    <div class="col-11 col-md-6 col-lg-4">
+      <div class="row justify-evenly align-center">
+        <h1 class="q-my-sm">Your deck</h1>
+      </div>
+    </div>
+  </q-page>
+</template>
