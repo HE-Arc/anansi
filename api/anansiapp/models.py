05d72485bb9d98e22d358296ad19761c7417dc44
Jeanne Michel <jeanne.michel@he-arc.ch>
2023-03-30 13:50:32 +0200

    models : ClozeCard, ResponseCard, FavouriteCardGame, Game, GamePlayer, Round, RoundResponseCard, GamePlayerResponseCard

--- api/anansiapp/models.py
+++ api/anansiapp/models.py
@@ -7,6 +7,8 @@ from django.dispatch import receiver
 
 
 class CardGame(models.Model):
+    """ CardGame model (jeu de cartes)
+    """
     user = models.ForeignKey(
         User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=False)
@@ -16,7 +18,56 @@ class CardGame(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+class ClozeCard(models.Model):
+    """ ClozeCard model (carte à trou)
+    """
+    cardgame = models.ForeignKey(
+        CardGame, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100, blank=False)
+    gap_index = models.IntegerField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ResponseCard(models.Model):
+    """ ResponseCard model (carte à réponse)
+    """
+    cardgame = models.ForeignKey(
+        CardGame, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FavouriteCardGame(models.Model):
+    """ FavouriteCardGame model (jeu de cartes favoris d'un utilisateur)
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cardgame = models.ForeignKey(CardGame, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Game(models.Model):
+    """ Game model (partie de jeu)
+    """
+    creator = models.ForeignKey(
+        "GamePlayer", on_delete=models.CASCADE, related_name='creator')
+    name = models.CharField(max_length=100)
+    winner = models.ForeignKey(
+        "GamePlayer", on_delete=models.CASCADE, null=True, related_name='winner')
+
+    cardgame = models.ForeignKey(CardGame, on_delete=models.CASCADE, null=True)
+
+    game_code = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 class GamePlayer(models.Model):
+    """ GamePlayer model (joueur d'une partie)
+    """
     game = models.ForeignKey(
         'Game', related_name='game', on_delete=models.CASCADE)
     user = models.ForeignKey(
@@ -25,18 +76,44 @@ class GamePlayer(models.Model):
     username = models.CharField(max_length=100)
     score = models.IntegerField(default=0)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-class Game(models.Model):
-    creator = models.ForeignKey(
-        "GamePlayer", on_delete=models.CASCADE, null=True, related_name='creator')
-    name = models.CharField(max_length=100)
-    winner = models.ForeignKey(
-        "GamePlayer", on_delete=models.CASCADE, null=True, related_name='winner')
 
-    # cardgame = models.ForeignKey(
-    #     CardGame, on_delete=models.CASCADE)
+class Round(models.Model):
+    """ Round model (tour d'une partie)
+    """
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    master = models.ForeignKey(
+        GamePlayer, on_delete=models.CASCADE, related_name='user_master')
+    cloze_card = models.ForeignKey(ClozeCard, on_delete=models.CASCADE)
+    response_card_winner = models.ForeignKey(
+        'RoundResponseCard', on_delete=models.CASCADE, null=True)
+    round_number = models.IntegerField(default=0)
 
-    game_code = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class RoundResponseCard(models.Model):
+    """ RoundResponseCard model (carte de réponse d'un tour d'une partie d'un joueur)
+    """
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    response_card = models.ForeignKey(ResponseCard, on_delete=models.CASCADE)
+    player = models.ForeignKey(
+        GamePlayer, on_delete=models.CASCADE, related_name='player')
+    is_winner = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class GamePlayerResponseCard(models.Model):
+    """ GamePlayerResponseCard model (carte de réponse d'un joueur d'une partie)
+    """
+    player = models.ForeignKey(GamePlayer, on_delete=models.CASCADE)
+    response_card = models.ForeignKey(ResponseCard, on_delete=models.CASCADE)
+    is_used = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
